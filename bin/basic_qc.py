bf8364aa88b3e2c9cf534875067413c9489b4f3e
Sylvain FORET <sylvain.foret@anu.edu.au>
2015-03-10 21:42:08 +1100

    Compute stats for a list of input files

--- bin/basic_qc.py
+++ bin/basic_qc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import argparse
+import os.path
+import sys
 
 import bee_tracker.qc_stats
 import bee_tracker.io_csv
@@ -9,6 +11,7 @@ def parseArgs():
     parser = argparse.ArgumentParser(description='Compute basic QC stats for bee movie')
     parser.add_argument('input',
                         metavar='FILE',
+                        nargs='+',
                         help='The input CSV file')
     parser.add_argument('-o',
                         '--outDir',
@@ -23,16 +26,23 @@ def parseArgs():
     return args
 
 def main(args):
-    bees  = bee_tracker.io_csv.loadBeesCSV(args.input)
-    for bee in bees.values():
-        bee.classify()
-    stats = [bee_tracker.qc_stats.BeesPerFrame,
-             bee_tracker.qc_stats.FramesPerBee,
-             bee_tracker.qc_stats.FramesPerPath,
-             bee_tracker.qc_stats.FramesBetweenPath,
-             bee_tracker.qc_stats.PathsPerBee,
-             bee_tracker.qc_stats.Classification]
-    bee_tracker.qc_stats.computeStats(stats, bees, args.outDir)
+    for path in args.input:
+        bees  = bee_tracker.io_csv.loadBeesCSV(path)
+        for bee in bees.values():
+            bee.classify()
+        name = os.path.basename(path)
+        stats = [bee_tracker.qc_stats.BeesPerFrame,
+                 bee_tracker.qc_stats.FramesPerBee,
+                 bee_tracker.qc_stats.FramesPerPath,
+                 bee_tracker.qc_stats.FramesBetweenPath,
+                 bee_tracker.qc_stats.PathsPerBee,
+                 bee_tracker.qc_stats.Classification]
+        outDir = os.path.join(args.outDir, name)
+        try:
+            os.makedirs(outDir)
+        except FileExistsError:
+            sys.stderr.write('Output directory "%s" already exists\n' % outDir)
+        bee_tracker.qc_stats.computeStats(stats, bees, outDir)
 
 if __name__ == '__main__':
     args  = parseArgs()
